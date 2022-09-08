d1b033e3007fc8fb6a8905a5a8c0f385f9eb0741
aguilar-lc <aguilar.lc@hotmail.com>
2022-09-08 18:22:25 -0300

    pasta exercicio python2

---
+++ Exercicios_Lista_Python_II/Exercicio01.py
@@ -0,0 +1,19 @@
+# Dada as seguintes informações: pessoa, cpf, nome e idade, crie uma classe onde teremos o retorno
+# do documento, o retorno do nome e verificação de tipo de pessoa, onde um método irá receber como
+# parâmetro "F" ou "N" para trazer fumante ou não fumente. Feito isso, crie uma instância e retorne 
+# esses valores.
+
+class Pessoa:
+    def __init__(self, nome, idade, cpf, tipo):
+        self.nome = nome
+        self.idade = idade
+        self.cpf = cpf
+        self.tipo = tipo
+    
+    def descricao(self):
+        if self.tipo == 'F':
+            print(f'{self.nome} tem {self.idade} anos, seu cpf é {self.cpf} e é fumante.')
+        if self.tipo == 'N':
+            print(f'{self.nome} tem {self.idade} anos, seu cpf é {self.cpf} e não é fumante.')
+        
+pessoa1 = Pessoa('João', '30', '123456789', 'N').descricao()
